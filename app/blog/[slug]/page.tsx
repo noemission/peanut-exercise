d028934dc12ef6c8294b37845a8c851b132cc1bc
Alex Michailidis <AMichailidis@buildinglink.com>
2024-03-09 16:20:22 +0200

    Create page for a blog post

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,13 +1,27 @@
+import Post from '@/components/BlogPost/Post';
+import fetchPost from 'lib/blog/post';
+import { notFound } from 'next/navigation';
+import { Metadata } from 'next';
+
 type Params = {
   params: {
     slug: string;
   };
 };
 
-export async function generateMetadata({ params }: Params) {
+export async function generateMetadata({ params }: Params):Promise<Metadata> {
+  const post = await fetchPost(params.slug);
+  if(post) {
+    return {
+      title: `Post: ${post.header.title}`,
+      description: post.description,
+    };
+  }
   return { title: `Post: ${params.slug}` };
 }
 
-export default function Page({ params }: Params) {
-  return <h1>Slug: {params.slug}</h1>;
+export default async function Page({ params }: Params) {
+  const post = await fetchPost(params.slug);
+  if(!post) notFound();
+  return <Post post={post} slug={params.slug}/>;
 }
